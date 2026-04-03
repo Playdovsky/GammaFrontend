27b288a7fad0854493daa1f86d66e9750e54898f
Mateusz <tubix21@wp.pl>
2026-04-04 01:27:14 +0200

    Contact form css

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -13,14 +13,14 @@ function submit() {
 </script>
 
 <template>
-  <h1>Contact us!</h1>
+  <h1 class="green">Contact us!</h1>
   <form @submit.prevent="submit">
     <label for="name">Your name</label>
-    <input id="name" v-model="contactForm.name" type="text" />
+    <input id="name" v-model="contactForm.name" type="text" required />
     <label for="email">email</label>
-    <input id="email" v-model="contactForm.email" type="email" />
+    <input id="email" v-model="contactForm.email" type="email" required />
     <label for="message">What do you want to tell us?</label>
-    <textarea id="message" v-model="contactForm.message"></textarea>
+    <textarea id="message" v-model="contactForm.message" required></textarea>
     <button type="submit">Send it!</button>
   </form>
 </template>
@@ -32,23 +32,51 @@ form {
 }
 
 label {
+  font-size: 18px;
   display: block;
   margin-bottom: 2px;
   margin-top: 8px;
+  color: var(--color-heading);
 }
 
 input[type='text'],
 input[type='email'],
 textarea {
-  width: 30%;
   padding: 8px;
   display: block;
   border-radius: 4px;
   box-sizing: border-box;
+  background-color: var(--color-background-soft);
+  color: var(--color-text);
+  border: 1px solid var(--color-border);
+}
+
+input[type='text'],
+input[type='email'],
+textarea,
+button {
+  width: 50%;
+  font-family:
+    Inter,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Fira Sans',
+    'Droid Sans',
+    'Helvetica Neue',
+    sans-serif;
+}
+
+textarea {
+  height: 100px;
 }
 
 button {
-  width: 30%;
+  font-size: 16px;
   background-color: #4caf50;
   color: white;
   padding: 14px;
